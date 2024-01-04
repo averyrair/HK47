38dec22e0a8044084b91f5efa6aba2a175807af8
avery <averyrair@gmail.com>
2024-01-03 19:50:24 -0500

    mini fix

--- interactions/Pazaak_Collection.js
+++ interactions/Pazaak_Collection.js
@@ -4,6 +4,10 @@ module.exports = {
     interactionID: 'manage_pazaak',
     execute: async (interaction) => {
 
+        if (interaction.message.interaction.user.id !== interaction.user.id) {
+            interaction.reply({content: 'Only the person who issued the original command can use this button.', ephemeral: true});
+            return;
+        }
         let manager = pazaak.createCardManager(interaction);
         interaction.update(await pazaak.renderCollection(manager));
     }
